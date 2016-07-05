52c3935ca8f810ea3d2320adb372944d2d3e4d2f
godchin <380580655@qq.com>
2016-07-05 18:12:08 +0800

    spinnabalestrinfg

--- tool/src/main/java/www/godchin/tool/MainActivity.java
+++ tool/src/main/java/www/godchin/tool/MainActivity.java
@@ -1,13 +1,23 @@
 package www.godchin.tool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import www.godchin.tool.info.DeviceUuidFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DeviceUuidFactory deviceUuidFactory = new DeviceUuidFactory(this);
+        String deviceId = deviceUuidFactory.getDeviceId(this);
+        Log.d(TAG, " deviceId" + deviceId);
+
+
     }
 }
